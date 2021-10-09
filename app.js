ecd528f33013562df84e9ecaed4fce3d510df8d2
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-10-09 15:21:46 +0530

    :sparkles: chore(Async/await way)

--- app.js
+++ app.js
@@ -5,16 +5,14 @@ jokeBtn.addEventListener("click", generateJoke);
 
 generateJoke();
 
-function generateJoke() {
+async function generateJoke() {
   const config = {
     headers: {
       Accept: "application/json",
     },
   };
 
-  fetch("https://icanhazdadjoke.com", config)
-    .then((response) => response.json())
-    .then((data) => {
-      jokeEl.innerHTML = data.joke;
-    });
+  const res = await fetch("https://icanhazdadjoke.com", config);
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
 }
